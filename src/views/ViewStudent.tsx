2ffc2747054d64d32d3acfbb6fe320a97e90a2a3
ideionatra <dioputra1423@gmail.com>
2025-02-16 20:44:26 +0800

    Create ViewStudent

--- src/views/ViewStudent.tsx
+++ src/views/ViewStudent.tsx
@@ -1,12 +1,108 @@
 import MainLayout from "@/components/layouts/MainLayout";
+import { Search } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const ViewStudent = () => {
   return (
     <MainLayout>
-      <h1 className="text-3xl font-semibold">Welcome to the ViewStudent!</h1>
-      <p className="mt-4 text-lg">
-        This is your main content area. You can add whatever you like here.
-      </p>
+      <div className="flex justify-between items-center px-6 py-4">
+        <div>
+          <h1 className="text-3xl font-bold text-green-500">Elisabet Ni Nyoman Rusmiati, S.Pd</h1>
+          <p className="text-xl ">Kelas yang diampu :</p>
+        </div>
+        <div className="flex gap-4 items-center p-3 bg-white rounded-md">
+          <label htmlFor="searchName"><Search className="size-6" /></label>
+          <input type="text" id="searchName" placeholder="Search by students name" className="w-72 text-sm outline-none placeholder:text-xs"/>
+        </div>
+      </div>
+
+      <div className="my-4 grid grid-cols-4 gap-y-10 px-20">
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">X TKP 1</CardTitle>
+            <CardContent className="text-gray-400 text-lg">31 siswa</CardContent>
+          </Card>
+        </a>
+        
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">X TKP 2</CardTitle>
+            <CardContent className="text-gray-400 text-lg">31 siswa</CardContent>
+          </Card>
+        </a>
+
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">XI RPL 1</CardTitle>
+            <CardContent className="text-gray-400 text-lg">35 siswa</CardContent>
+          </Card>
+        </a>
+
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">XI RPL 2</CardTitle>
+            <CardContent className="text-gray-400 text-lg">35 siswa</CardContent>
+          </Card>
+        </a>
+
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">XI RPL 3</CardTitle>
+            <CardContent className="text-gray-400 text-lg">36 siswa</CardContent>
+          </Card>
+        </a>
+
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">XI TKP 1</CardTitle>
+            <CardContent className="text-gray-400 text-lg">26 siswa</CardContent>
+          </Card>
+        </a>
+
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">XI TKP 2</CardTitle>
+            <CardContent className="text-gray-400 text-lg">27 siswa</CardContent>
+          </Card>
+        </a>
+
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">XII BKP 1</CardTitle>
+            <CardContent className="text-gray-400 text-lg">33 siswa</CardContent>
+          </Card>
+        </a>
+
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">XII BKP 2</CardTitle>
+            <CardContent className="text-gray-400 text-lg">31 siswa</CardContent>
+          </Card>
+        </a>
+
+        <a href="">
+          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+            <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
+            <CardTitle className="mt-8 text-3xl font-semibold">XII RPL 3</CardTitle>
+            <CardContent className="text-gray-400 text-lg">38 siswa</CardContent>
+          </Card>
+        </a>
+      </div>
+      
     </MainLayout>
   );
 };
