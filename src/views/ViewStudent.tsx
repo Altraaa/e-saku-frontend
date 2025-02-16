f15e46f2feaf330d86be608bb0deafe8f7a08280
ideionatra <dioputra1423@gmail.com>
2025-02-16 21:29:46 +0800

    Added Responsive To ViewStudent

--- src/views/ViewStudent.tsx
+++ src/views/ViewStudent.tsx
@@ -10,7 +10,7 @@ import {
 const ViewStudent = () => {
   return (
     <MainLayout>
-      <div className="flex justify-between items-center px-6 py-4">
+      <div className="flex flex-col items-start gap-2 lg:flex-row lg:justify-between lg:items-center px-6 py-4">
         <div>
           <h1 className="text-3xl font-bold text-green-500">Elisabet Ni Nyoman Rusmiati, S.Pd</h1>
           <p className="text-xl ">Kelas yang diampu :</p>
@@ -21,9 +21,9 @@ const ViewStudent = () => {
         </div>
       </div>
 
-      <div className="my-4 grid grid-cols-4 gap-y-10 px-20">
+      <div className="my-4 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-10 px-20">
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">X TKP 1</CardTitle>
             <CardContent className="text-gray-400 text-lg">31 siswa</CardContent>
@@ -31,7 +31,7 @@ const ViewStudent = () => {
         </a>
         
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">X TKP 2</CardTitle>
             <CardContent className="text-gray-400 text-lg">31 siswa</CardContent>
@@ -39,7 +39,7 @@ const ViewStudent = () => {
         </a>
 
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">XI RPL 1</CardTitle>
             <CardContent className="text-gray-400 text-lg">35 siswa</CardContent>
@@ -47,7 +47,7 @@ const ViewStudent = () => {
         </a>
 
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">XI RPL 2</CardTitle>
             <CardContent className="text-gray-400 text-lg">35 siswa</CardContent>
@@ -55,7 +55,7 @@ const ViewStudent = () => {
         </a>
 
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">XI RPL 3</CardTitle>
             <CardContent className="text-gray-400 text-lg">36 siswa</CardContent>
@@ -63,7 +63,7 @@ const ViewStudent = () => {
         </a>
 
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">XI TKP 1</CardTitle>
             <CardContent className="text-gray-400 text-lg">26 siswa</CardContent>
@@ -71,7 +71,7 @@ const ViewStudent = () => {
         </a>
 
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">XI TKP 2</CardTitle>
             <CardContent className="text-gray-400 text-lg">27 siswa</CardContent>
@@ -79,7 +79,7 @@ const ViewStudent = () => {
         </a>
 
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">XII BKP 1</CardTitle>
             <CardContent className="text-gray-400 text-lg">33 siswa</CardContent>
@@ -87,7 +87,7 @@ const ViewStudent = () => {
         </a>
 
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">XII BKP 2</CardTitle>
             <CardContent className="text-gray-400 text-lg">31 siswa</CardContent>
@@ -95,7 +95,7 @@ const ViewStudent = () => {
         </a>
 
         <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">XII RPL 3</CardTitle>
             <CardContent className="text-gray-400 text-lg">38 siswa</CardContent>
