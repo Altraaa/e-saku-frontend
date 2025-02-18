38af078742712f808f57ded29bab9e64120cc187
ideionatra <dioputra1423@gmail.com>
2025-02-18 22:57:10 +0800

    Responsive for Student Page

--- src/views/ViewStudent.tsx
+++ src/views/ViewStudent.tsx
@@ -9,7 +9,7 @@ import {
 const ViewStudent = () => {
   return (
     <>
-      <div className="flex justify-between items-center px-6 py-4">
+      <div className="flex flex-col items-start gap-2 lg:flex-row lg:justify-between lg:items-center">
         <div>
           <h1 className="text-3xl font-bold text-green-500">Elisabet Ni Nyoman Rusmiati, S.Pd</h1>
           <p className="text-xl ">Kelas yang diampu :</p>
@@ -20,9 +20,9 @@ const ViewStudent = () => {
         </div>
       </div>
 
-      <div className="my-4 grid grid-cols-4 gap-y-10 px-20">
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+      <div className="my-4 grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-10 px-20 mt-10">
+        <a href="/class?xtkp1" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xtkp1" className="group-hover:text-green-500 transition-all duration-200">X TKP 1</a> 
@@ -31,8 +31,8 @@ const ViewStudent = () => {
           </Card>
         </a>
         
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xtkp2" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xtkp2" className="group-hover:text-green-500 transition-all duration-200">X TKP 2</a>
@@ -41,8 +41,8 @@ const ViewStudent = () => {
           </Card>
         </a>
 
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xirpl1" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xirpl1" className="group-hover:text-green-500 transition-all duration-200">XI RPL 1</a>
@@ -51,8 +51,8 @@ const ViewStudent = () => {
           </Card>
         </a>
 
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xirpl2" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xirpl2" className="group-hover:text-green-500 transition-all duration-200">XI RPL 2</a>
@@ -61,8 +61,8 @@ const ViewStudent = () => {
           </Card>
         </a>
 
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xirpl3" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xirpl3" className="group-hover:text-green-500 transition-all duration-200">XI RPL 3</a>
@@ -71,8 +71,8 @@ const ViewStudent = () => {
           </Card>
         </a>
 
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xitkp1" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xitkp1" className="group-hover:text-green-500 transition-all duration-200">XI TKP 1</a>
@@ -81,8 +81,8 @@ const ViewStudent = () => {
           </Card>
         </a>
 
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xitkp2" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xitkp2" className="group-hover:text-green-500 transition-all duration-200">XI TKP 2</a>
@@ -91,8 +91,8 @@ const ViewStudent = () => {
           </Card>
         </a>
 
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xiibkp1" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xiibkp1" className="group-hover:text-green-500 transition-all duration-200">XII BKP 1</a>
@@ -101,8 +101,8 @@ const ViewStudent = () => {
           </Card>
         </a>
 
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xiibkp2" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xiibkp2" className="group-hover:text-green-500 transition-all duration-200">XII BKP 2</a>
@@ -111,8 +111,8 @@ const ViewStudent = () => {
           </Card>
         </a>
 
-        <a href="">
-          <Card className="bg-white shadow-md w-96 py-24 flex flex-col items-center">
+        <a href="/class?xiirpl3" className="group">
+          <Card className="bg-white shadow-md w-72 2xl:w-96 py-24 flex flex-col items-center group-hover:shadow-green-500 transition-all duration-200">
             <CardHeader className="w-40 h-40 rounded-full bg-gray-400" />
             <CardTitle className="mt-8 text-3xl font-semibold">
               <a href="/class?xiirpl3" className="group-hover:text-green-500 transition-all duration-200">XII RPL 3</a>
